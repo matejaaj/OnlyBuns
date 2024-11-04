1da96898cac6793b9d86448ffb811fcea4df06ce
matejaaj <mateja.odzaci@gmail.com>
2024-11-04 22:05:16 +0100

    init

---
+++ backend/only-buns/src/main/java/com/example/onlybuns/OnlyBunsApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlybuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyBunsApplication.class, args);
+	}
+
+}
