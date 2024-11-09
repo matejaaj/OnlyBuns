2be5881ca2794608e3de26f10bb1c24de9e623b3
matejaaj <mateja.odzaci@gmail.com>
2024-11-09 18:31:52 +0100

    fix: merge fix

--- only-buns-be/src/main/java/com/example/onlybunsbe/model/User.java
+++ only-buns-be/src/main/java/com/example/onlybunsbe/model/User.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,20 +18,14 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-
+@Getter
+@Setter
 @Table(name="users")
 public class User implements UserDetails {
     @Id
-    @Column(name = "id", nullable = false)
-    private Integer id;
-
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
+
 
     @NotNull
     @Size(min = 3, max = 100)
@@ -75,97 +71,10 @@ public class User implements UserDetails {
     @JoinColumn(name = "role_id") // Naziv kolone u User tabeli koja pokazuje na Role
     private Role role;
 
-    // Getteri i setteri
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-        this.setLastPasswordResetDate(new Timestamp(new Date().getTime()));
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public String getActivationToken() {
-        return activationToken;
-    }
-
-    public void setActivationToken(String activationToken) {
-        this.activationToken = activationToken;
-    }
-
-    public Timestamp getLastPasswordResetDate() {
-        return lastPasswordResetDate;
-    }
-
-    public void setLastPasswordResetDate(Timestamp lastPasswordResetDate) {
-        this.lastPasswordResetDate = lastPasswordResetDate;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
     @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(role); // Pretvara ulogu u listu sa jednim elementom
+        return List.of(role);
     }
 
     @JsonIgnore
---
+++ only-buns-be/src/main/resources/data-postgres.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.roles(
+	id, name)
+	VALUES (1, 'ROLE_ADMIN');
+
+INSERT INTO public.roles(
+	id, name)
+	VALUES (2, 'ROLE_USER');
