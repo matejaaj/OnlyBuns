ca99834fd8d85dad18fffdf9d13abfeb16fedc2c
matejaaj <mateja.odzaci@gmail.com>
2024-12-24 19:28:08 +0100

    fix: small fix

--- only-buns-be/src/main/java/com/example/onlybunsbe/Config/WebSecurityConfig.java
+++ only-buns-be/src/main/java/com/example/onlybunsbe/Config/WebSecurityConfig.java
@@ -102,7 +102,8 @@ public class WebSecurityConfig {
                 "/api-docs/**",
                 "/swagger-resources/**",
                 "/webjars/**",
-                "/uploads/**"
+                "/uploads/**",
+                "/actuator/prometheus"
         );
     }
 }
